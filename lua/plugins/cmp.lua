548e926866853c4232c29bbe74d6d8f3278d28f7
Quinn <3379314+quinnyo@users.noreply.github.com>
2025-05-14 23:14:34 +1000

    Start fixing autocomplete
    - Unmap <CR> as complete accept, use <Tab> instead.
    	- Use nvim-cmp or whatever instead of the other thing, maybe for no reason, who could say.
    	- Had to override the plugin config I guess, because the other six ways to set keys didn't work.
    		- this includes **simply** setting mapping to `false`
    	- still appears automatically
    	- still automatically selects a suggestion
    	- still suggests plaintext words, comments
    	- still shows glitchy mess, text "through" the popup
    - Remove plugin (mini.pairs) that inserts brackets/quotes
    - Remove plugin (flash) that **changes what 's' does** (seriously!?)

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "hrsh7th/cmp-emoji" },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+
+      opts.mapping = cmp.mapping.preset.insert({
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<Tab>"] = LazyVim.cmp.confirm(),
+      })
+
+      table.insert(opts.sources, { name = "emoji" })
+    end,
+  },
+}
+
